11fa671b915bb6d66d82fa4000833f432a811005
CameronDeweerd <87665700+CameronDeweerd@users.noreply.github.com>
2021-11-15 11:04:14 -0500

    Webmail setup

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template, request, redirect
+import smtplib
+import ssl
 from .weight_check import *
 
 portfolio = Blueprint('portfolio', __name__, template_folder='templates', static_folder='static')
@@ -29,3 +31,36 @@ def weight():
         return render_template("weight.html", final_group=final_group, final_options=final_options)
     else:
         return render_template("weight.html")
+
+
+@portfolio.route("/contactForm", methods=["POST"])
+def contact():
+    port = 465  # For SSL
+    sender_email = "cameronDwebmail@gmail.com"
+    receiver_email = "cameron.deweerd@gmail.com"
+    name = request.form.get("contact-name")
+    sender = request.form.get("contact-email")
+    text = request.form.get("contact-message")
+    org = request.form.get("contact-org")
+
+    print(request.form.get("contact-name"))
+
+    if org:
+        name = f"{name} at {org}"
+
+
+    message = f"""Subject: New message from {name}
+
+    {name} sends the following:
+    {text}
+
+    reply back at: {sender}"""
+
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(sender_email, "mailService123")
+        server.sendmail(sender_email, receiver_email, message)
+
+    return redirect("/")
